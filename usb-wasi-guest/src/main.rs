3a87c78602d36f68b67e74c8b01f1a98183b9e95
Robbe Leroy <leroyrobbe@icloud.com>
2025-04-09 14:17:08 +0200

    guest
    # Conflicts:
    #	usb-wasi-host/Cargo.toml
    #	usb-wasi-host/src/main.rs

--- usb-wasi-guest/src/main.rs
+++ usb-wasi-guest/src/main.rs
@@ -1,5 +1,20 @@
+use wit_bindgen::generate;
+use crate::component::usb::usb::get_context;
+
+generate!({
+    world: "guest",
+    path: "../wit",
+});
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
+    match get_context() {
+        Ok(ctx) => {
+            println!("Context: {:?}", ctx);
+        },
+        Err(e) => {
+            println!("Error: {:?}", e);
+        }
+    }
     println!("Hello, world!");
 }
--- usb-wasi-host/src/main.rs
+++ usb-wasi-host/src/main.rs
@@ -1,8 +1,14 @@
+use crate::component::usb::events::DeviceConnectionEvent;
+use crate::component::usb::usb::{
+    ConfigDescriptor, DeviceDescriptor, DeviceHandleError, Duration, HostContext, HostDeviceHandle,
+    HostUsbDevice, Language, Speed,
+};
 use std::future::Future;
 use wasmtime::component::*;
-use wasmtime::Result;
+use wasmtime::{Config, Result};
 use wasmtime::{Engine, Store};
-use component::usb::usb::{HostContext, HostUsbDevice, HostDeviceHandle};
+use wasmtime_wasi::bindings::Command;
+use wasmtime_wasi::{IoView, WasiCtx, WasiCtxBuilder, WasiView};
 
 pub struct UsbDevice {}
 pub struct UsbContext {}
@@ -11,6 +17,8 @@ pub struct DeviceHandle {}
 bindgen!({
     world: "imports",
     path: "../wit",
+
+
     with: {
         "component:usb/usb/usb-device": UsbDevice,
         "component:usb/usb/context": UsbContext,
@@ -20,20 +28,280 @@ bindgen!({
     async: true
 });
 
-#[derive(Default)]
 struct MyState {
     table: ResourceTable,
+    ctx: WasiCtx,
 }
 
-impl component::usb::usb::Host for MyState {}
+impl MyState {
+    pub fn new() -> Self {
+        Self {
+            table: ResourceTable::new(),
+            ctx: WasiCtxBuilder::new().inherit_stdio().build(),
+        }
+    }
+}
 
-impl HostContext for MyState {
+impl component::usb::types::Host for MyState {}
+impl component::usb::descriptors::Host for MyState {}
 
-    fn devices(&mut self, self_: Resource<UsbContext>) -> Result<Result<Vec<Resource<UsbDevice>>>> {
+impl HostDeviceHandle for MyState {
+    async fn device(&mut self, self_: Resource<DeviceHandle>) -> Result<Resource<UsbDevice>> {
         todo!()
     }
 
-    async fn open_device_with_vid_pid(&mut self, self_: Resource<UsbContext>, vendor_id: u16, product_id: u16) -> Result<Option<Resource<DeviceHandle>>> {
+    async fn active_configuration(
+        &mut self,
+        self_: Resource<DeviceHandle>,
+    ) -> Result<std::result::Result<u8, ()>> {
+        todo!()
+    }
+
+    async fn set_active_configuration(
+        &mut self,
+        self_: Resource<DeviceHandle>,
+        config: u8,
+    ) -> Result<std::result::Result<(), ()>> {
+        todo!()
+    }
+
+    async fn unconfigure(
+        &mut self,
+        self_: Resource<DeviceHandle>,
+    ) -> Result<std::result::Result<(), ()>> {
+        todo!()
+    }
+
+    async fn reset(
+        &mut self,
+        self_: Resource<DeviceHandle>,
+    ) -> Result<std::result::Result<(), ()>> {
+        todo!()
+    }
+
+    async fn clear_halt(
+        &mut self,
+        self_: Resource<DeviceHandle>,
+        endpoint: u8,
+    ) -> Result<std::result::Result<(), ()>> {
+        todo!()
+    }
+
+    async fn kernel_driver_active(
+        &mut self,
+        self_: Resource<DeviceHandle>,
+    ) -> Result<std::result::Result<bool, ()>> {
+        todo!()
+    }
+
+    async fn detach_kernel_driver(
+        &mut self,
+        self_: Resource<DeviceHandle>,
+    ) -> Result<std::result::Result<(), ()>> {
+        todo!()
+    }
+
+    async fn attach_kernel_driver(
+        &mut self,
+        self_: Resource<DeviceHandle>,
+    ) -> Result<std::result::Result<(), ()>> {
+        todo!()
+    }
+
+    async fn set_auto_attach_detach_kernel_driver(
+        &mut self,
+        self_: Resource<DeviceHandle>,
+        auto: bool,
+    ) -> Result<std::result::Result<(), ()>> {
+        todo!()
+    }
+
+    async fn claim_interface(
+        &mut self,
+        self_: Resource<DeviceHandle>,
+        iface: u8,
+    ) -> Result<std::result::Result<(), ()>> {
+        todo!()
+    }
+
+    async fn release_interface(
+        &mut self,
+        self_: Resource<DeviceHandle>,
+        iface: u8,
+    ) -> Result<std::result::Result<(), ()>> {
+        todo!()
+    }
+
+    async fn set_interface_alt_setting(
+        &mut self,
+        self_: Resource<DeviceHandle>,
+        iface: u8,
+        alt_setting: u8,
+    ) -> Result<std::result::Result<(), ()>> {
+        todo!()
+    }
+
+    async fn read_interrupt(
+        &mut self,
+        self_: Resource<DeviceHandle>,
+        endpoint: u8,
+        timeout: Duration,
+    ) -> Result<std::result::Result<(u64, Vec<u8>), DeviceHandleError>> {
+        todo!()
+    }
+
+    async fn write_interrupt(
+        &mut self,
+        self_: Resource<DeviceHandle>,
+        endpoint: u8,
+        data: Vec<u8>,
+        timeout: Duration,
+    ) -> Result<std::result::Result<u64, DeviceHandleError>> {
+        todo!()
+    }
+
+    async fn read_bulk(
+        &mut self,
+        self_: Resource<DeviceHandle>,
+        endpoint: u8,
+        max_size: u64,
+        timeout: Duration,
+    ) -> Result<std::result::Result<(u64, Vec<u8>), DeviceHandleError>> {
+        todo!()
+    }
+
+    async fn write_bulk(
+        &mut self,
+        self_: Resource<DeviceHandle>,
+        endpoint: u8,
+        data: Vec<u8>,
+        timeout: Duration,
+    ) -> Result<std::result::Result<u64, DeviceHandleError>> {
+        todo!()
+    }
+
+    async fn read_control(
+        &mut self,
+        self_: Resource<DeviceHandle>,
+        request_type: u8,
+        request: u8,
+        value: u16,
+        index: u16,
+        max_size: u16,
+        timeout: Duration,
+    ) -> Result<std::result::Result<(u64, Vec<u8>), DeviceHandleError>> {
+        todo!()
+    }
+
+    async fn write_control(
+        &mut self,
+        self_: Resource<DeviceHandle>,
+        request_type: u8,
+        request: u8,
+        value: u16,
+        index: u16,
+        data: Vec<u8>,
+        timeout: Duration,
+    ) -> Result<std::result::Result<u64, DeviceHandleError>> {
+        todo!()
+    }
+
+    async fn read_languages(
+        &mut self,
+        self_: Resource<DeviceHandle>,
+        timeout: Duration,
+    ) -> Result<std::result::Result<Vec<Language>, ()>> {
+        todo!()
+    }
+
+    async fn read_string_descriptor_ascii(
+        &mut self,
+        self_: Resource<DeviceHandle>,
+        index: u8,
+    ) -> Result<std::result::Result<String, ()>> {
+        todo!()
+    }
+
+    async fn read_string_descriptor(
+        &mut self,
+        self_: Resource<DeviceHandle>,
+        language: Language,
+        index: u8,
+        timeout: Duration,
+    ) -> Result<std::result::Result<String, ()>> {
+        todo!()
+    }
+
+    async fn read_manufacturer_string_ascii(
+        &mut self,
+        self_: Resource<DeviceHandle>,
+        device: DeviceDescriptor,
+    ) -> Result<()> {
+        todo!()
+    }
+
+    async fn drop(&mut self, rep: Resource<DeviceHandle>) -> Result<()> {
+        todo!()
+    }
+}
+
+impl HostUsbDevice for MyState {
+    async fn device_descriptor(
+        &mut self,
+        self_: Resource<UsbDevice>,
+    ) -> Result<std::result::Result<DeviceDescriptor, ()>> {
+        todo!()
+    }
+
+    async fn config_descriptor(
+        &mut self,
+        self_: Resource<UsbDevice>,
+    ) -> Result<std::result::Result<ConfigDescriptor, ()>> {
+        todo!()
+    }
+
+    async fn bus_number(&mut self, self_: Resource<UsbDevice>) -> Result<u8> {
+        todo!()
+    }
+
+    async fn address(&mut self, self_: Resource<UsbDevice>) -> Result<u8> {
+        todo!()
+    }
+
+    async fn speed(&mut self, self_: Resource<UsbDevice>) -> Result<Speed> {
+        todo!()
+    }
+
+    async fn open(
+        &mut self,
+        self_: Resource<UsbDevice>,
+    ) -> Result<std::result::Result<Resource<DeviceHandle>, ()>> {
+        todo!()
+    }
+
+    async fn port_number(&mut self, self_: Resource<UsbDevice>) -> Result<u8> {
+        todo!()
+    }
+
+    async fn drop(&mut self, rep: Resource<UsbDevice>) -> Result<()> {
+        todo!()
+    }
+}
+
+impl HostContext for MyState {
+    async fn devices(
+        &mut self,
+        self_: Resource<UsbContext>,
+    ) -> Result<std::result::Result<Vec<Resource<UsbDevice>>, ()>> {
+        todo!()
+    }
+
+    async fn open_device_with_vid_pid(
+        &mut self,
+        self_: Resource<UsbContext>,
+        vendor_id: u16,
+        product_id: u16,
+    ) -> Result<Option<Resource<DeviceHandle>>> {
         todo!()
     }
 
@@ -42,16 +310,56 @@ impl HostContext for MyState {
     }
 }
 
-impl HostUsbDevice for MyState {}
-impl HostDeviceHandle for MyState {}
+impl component::usb::events::Host for MyState {
+    async fn update(&mut self) -> Result<StreamReader<u8>> {
+        // an eventhandler should be added to the rusb hotplugbuilder,
+        // these events should be sent in some way to a buffer, for example mpsc
+        // the update function should then return the next event from the buffer
+        todo!()
+    }
+}
 
+impl component::usb::usb::Host for MyState {
+    async fn get_context(&mut self) -> Result<std::result::Result<Resource<UsbContext>, ()>> {
+        // make a context and return it, if the context already exists, return it
+        Result::Ok(Err(()))
+    }
+}
 
-fn main() -> wasmtime::Result<()> {
+impl IoView for MyState {
+    fn table(&mut self) -> &mut ResourceTable {
+        &mut self.table
+    }
+}
+
+impl WasiView for MyState {
+    fn ctx(&mut self) -> &mut WasiCtx {
+        &mut self.ctx
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
     // Compile the `Component` that is being run for the application.
-    let engine = Engine::default();
-    let component = Component::from_file(&engine, "target/wasm32-wasi/debug/usb.wasm")?;
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Usage: {} <path_to_component>", args[0]);
+        std::process::exit(1);
+    }
+    let component_path = &args[1];
+    let engine = Engine::new(Config::new().async_support(true).wasm_component_model_async(true))?;
+    let component = Component::from_file(&engine, component_path)?;
     let mut linker = Linker::new(&engine);
     Imports::add_to_linker(&mut linker, |state: &mut MyState| state)?;
-    let mut store = Store::new(&engine, MyState::default());
+    wasmtime_wasi::add_to_linker_async(&mut linker)?;
+    let mut store = Store::new(&engine, MyState::new());
+    let command = Command::instantiate_async(&mut store, &component, &linker).await?;
+    command.wasi_cli_run().call_run(store).await?.unwrap();
     Ok(())
 }
+
+/*
+    libusb bekijken webusb, taal die programma zelf gebruken zie compileren naar webusb
+    gelijkaardige backend voor libusb voor wasi usb
+    applicatie compileren met libusb compilenren naar wasi en wasm
+ */
