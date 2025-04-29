583b9136591bdb9a79e70fd3297e3fd5be96a625
Robbe Leroy <leroyrobbe@icloud.com>
2025-04-29 23:17:08 +0200

    Implement async USB transfer methods and enhance device handling

--- usb-wasi-host/src/main.rs
+++ usb-wasi-host/src/main.rs
@@ -1,31 +1,40 @@
-use crate::component::usb::events::DeviceConnectionEvent;
-use crate::component::usb::usb::{
-    ConfigDescriptor, DeviceDescriptor, DeviceHandleError, Duration, HostContext, HostDeviceHandle,
-    HostUsbDevice, Language, Speed,
-};
-use std::future::Future;
+use std::sync::Arc;
+use std::sync::atomic::{AtomicBool, Ordering};
+use crate::component::usb::configuration::ConfigValue;
+use crate::component::usb::descriptors::ConfigurationDescriptor;
+use crate::component::usb::device::{HostDeviceHandle, HostUsbDevice, TransferOptions, TransferSetup};
+use crate::component::usb::errors::LibusbError;
+use crate::component::usb::transfers::{HostTransfer, Transfer};
+use libusb_sys::{libusb_attach_kernel_driver, libusb_cancel_transfer, libusb_clear_halt, libusb_close, libusb_config_descriptor, libusb_detach_kernel_driver, libusb_device, libusb_device_handle, libusb_get_config_descriptor, libusb_kernel_driver_active, libusb_reset_device, libusb_transfer, libusb_unref_device};
 use wasmtime::component::*;
 use wasmtime::{Config, Result};
 use wasmtime::{Engine, Store};
 use wasmtime_wasi::bindings::Command;
 use wasmtime_wasi::{IoView, WasiCtx, WasiCtxBuilder, WasiView};
 
-pub struct UsbDevice {}
-pub struct UsbContext {}
-pub struct DeviceHandle {}
+pub struct UsbTransfer {
+    transfer: *mut libusb_transfer,
+    completed: Arc<AtomicBool>,
+
+}
+pub struct UsbDevice {
+    pub device: *mut libusb_device
+}
+pub struct UsbDeviceHandle {
+    handle: *mut libusb_device_handle
+}
 
 bindgen!({
-    world: "imports",
+    world: "host",
     path: "../wit",
-
-
     with: {
-        "component:usb/usb/usb-device": UsbDevice,
-        "component:usb/usb/context": UsbContext,
-        "component:usb/usb/device-handle": DeviceHandle,
+        "component:usb/transfers/transfer": UsbTransfer,
+        "component:usb/device/usb-device": UsbDevice,
+        "component:usb/device/device-handle": UsbDeviceHandle,
     },
     trappable_imports: true,
     async: true
+    
 });
 
 struct MyState {
@@ -42,290 +51,6 @@ impl MyState {
     }
 }
 
-impl component::usb::types::Host for MyState {}
-impl component::usb::descriptors::Host for MyState {}
-
-impl HostDeviceHandle for MyState {
-    async fn device(&mut self, self_: Resource<DeviceHandle>) -> Result<Resource<UsbDevice>> {
-        todo!()
-    }
-
-    async fn active_configuration(
-        &mut self,
-        self_: Resource<DeviceHandle>,
-    ) -> Result<std::result::Result<u8, ()>> {
-        todo!()
-    }
-
-    async fn set_active_configuration(
-        &mut self,
-        self_: Resource<DeviceHandle>,
-        config: u8,
-    ) -> Result<std::result::Result<(), ()>> {
-        todo!()
-    }
-
-    async fn unconfigure(
-        &mut self,
-        self_: Resource<DeviceHandle>,
-    ) -> Result<std::result::Result<(), ()>> {
-        todo!()
-    }
-
-    async fn reset(
-        &mut self,
-        self_: Resource<DeviceHandle>,
-    ) -> Result<std::result::Result<(), ()>> {
-        todo!()
-    }
-
-    async fn clear_halt(
-        &mut self,
-        self_: Resource<DeviceHandle>,
-        endpoint: u8,
-    ) -> Result<std::result::Result<(), ()>> {
-        todo!()
-    }
-
-    async fn kernel_driver_active(
-        &mut self,
-        self_: Resource<DeviceHandle>,
-    ) -> Result<std::result::Result<bool, ()>> {
-        todo!()
-    }
-
-    async fn detach_kernel_driver(
-        &mut self,
-        self_: Resource<DeviceHandle>,
-    ) -> Result<std::result::Result<(), ()>> {
-        todo!()
-    }
-
-    async fn attach_kernel_driver(
-        &mut self,
-        self_: Resource<DeviceHandle>,
-    ) -> Result<std::result::Result<(), ()>> {
-        todo!()
-    }
-
-    async fn set_auto_attach_detach_kernel_driver(
-        &mut self,
-        self_: Resource<DeviceHandle>,
-        auto: bool,
-    ) -> Result<std::result::Result<(), ()>> {
-        todo!()
-    }
-
-    async fn claim_interface(
-        &mut self,
-        self_: Resource<DeviceHandle>,
-        iface: u8,
-    ) -> Result<std::result::Result<(), ()>> {
-        todo!()
-    }
-
-    async fn release_interface(
-        &mut self,
-        self_: Resource<DeviceHandle>,
-        iface: u8,
-    ) -> Result<std::result::Result<(), ()>> {
-        todo!()
-    }
-
-    async fn set_interface_alt_setting(
-        &mut self,
-        self_: Resource<DeviceHandle>,
-        iface: u8,
-        alt_setting: u8,
-    ) -> Result<std::result::Result<(), ()>> {
-        todo!()
-    }
-
-    async fn read_interrupt(
-        &mut self,
-        self_: Resource<DeviceHandle>,
-        endpoint: u8,
-        timeout: Duration,
-    ) -> Result<std::result::Result<(u64, Vec<u8>), DeviceHandleError>> {
-        todo!()
-    }
-
-    async fn write_interrupt(
-        &mut self,
-        self_: Resource<DeviceHandle>,
-        endpoint: u8,
-        data: Vec<u8>,
-        timeout: Duration,
-    ) -> Result<std::result::Result<u64, DeviceHandleError>> {
-        todo!()
-    }
-
-    async fn read_bulk(
-        &mut self,
-        self_: Resource<DeviceHandle>,
-        endpoint: u8,
-        max_size: u64,
-        timeout: Duration,
-    ) -> Result<std::result::Result<(u64, Vec<u8>), DeviceHandleError>> {
-        todo!()
-    }
-
-    async fn write_bulk(
-        &mut self,
-        self_: Resource<DeviceHandle>,
-        endpoint: u8,
-        data: Vec<u8>,
-        timeout: Duration,
-    ) -> Result<std::result::Result<u64, DeviceHandleError>> {
-        todo!()
-    }
-
-    async fn read_control(
-        &mut self,
-        self_: Resource<DeviceHandle>,
-        request_type: u8,
-        request: u8,
-        value: u16,
-        index: u16,
-        max_size: u16,
-        timeout: Duration,
-    ) -> Result<std::result::Result<(u64, Vec<u8>), DeviceHandleError>> {
-        todo!()
-    }
-
-    async fn write_control(
-        &mut self,
-        self_: Resource<DeviceHandle>,
-        request_type: u8,
-        request: u8,
-        value: u16,
-        index: u16,
-        data: Vec<u8>,
-        timeout: Duration,
-    ) -> Result<std::result::Result<u64, DeviceHandleError>> {
-        todo!()
-    }
-
-    async fn read_languages(
-        &mut self,
-        self_: Resource<DeviceHandle>,
-        timeout: Duration,
-    ) -> Result<std::result::Result<Vec<Language>, ()>> {
-        todo!()
-    }
-
-    async fn read_string_descriptor_ascii(
-        &mut self,
-        self_: Resource<DeviceHandle>,
-        index: u8,
-    ) -> Result<std::result::Result<String, ()>> {
-        todo!()
-    }
-
-    async fn read_string_descriptor(
-        &mut self,
-        self_: Resource<DeviceHandle>,
-        language: Language,
-        index: u8,
-        timeout: Duration,
-    ) -> Result<std::result::Result<String, ()>> {
-        todo!()
-    }
-
-    async fn read_manufacturer_string_ascii(
-        &mut self,
-        self_: Resource<DeviceHandle>,
-        device: DeviceDescriptor,
-    ) -> Result<()> {
-        todo!()
-    }
-
-    async fn drop(&mut self, rep: Resource<DeviceHandle>) -> Result<()> {
-        todo!()
-    }
-}
-
-impl HostUsbDevice for MyState {
-    async fn device_descriptor(
-        &mut self,
-        self_: Resource<UsbDevice>,
-    ) -> Result<std::result::Result<DeviceDescriptor, ()>> {
-        todo!()
-    }
-
-    async fn config_descriptor(
-        &mut self,
-        self_: Resource<UsbDevice>,
-    ) -> Result<std::result::Result<ConfigDescriptor, ()>> {
-        todo!()
-    }
-
-    async fn bus_number(&mut self, self_: Resource<UsbDevice>) -> Result<u8> {
-        todo!()
-    }
-
-    async fn address(&mut self, self_: Resource<UsbDevice>) -> Result<u8> {
-        todo!()
-    }
-
-    async fn speed(&mut self, self_: Resource<UsbDevice>) -> Result<Speed> {
-        todo!()
-    }
-
-    async fn open(
-        &mut self,
-        self_: Resource<UsbDevice>,
-    ) -> Result<std::result::Result<Resource<DeviceHandle>, ()>> {
-        todo!()
-    }
-
-    async fn port_number(&mut self, self_: Resource<UsbDevice>) -> Result<u8> {
-        todo!()
-    }
-
-    async fn drop(&mut self, rep: Resource<UsbDevice>) -> Result<()> {
-        todo!()
-    }
-}
-
-impl HostContext for MyState {
-    async fn devices(
-        &mut self,
-        self_: Resource<UsbContext>,
-    ) -> Result<std::result::Result<Vec<Resource<UsbDevice>>, ()>> {
-        todo!()
-    }
-
-    async fn open_device_with_vid_pid(
-        &mut self,
-        self_: Resource<UsbContext>,
-        vendor_id: u16,
-        product_id: u16,
-    ) -> Result<Option<Resource<DeviceHandle>>> {
-        todo!()
-    }
-
-    async fn drop(&mut self, rep: Resource<UsbContext>) -> Result<()> {
-        todo!()
-    }
-}
-
-impl component::usb::events::Host for MyState {
-    async fn update(&mut self) -> Result<StreamReader<u8>> {
-        // an eventhandler should be added to the rusb hotplugbuilder,
-        // these events should be sent in some way to a buffer, for example mpsc
-        // the update function should then return the next event from the buffer
-        todo!()
-    }
-}
-
-impl component::usb::usb::Host for MyState {
-    async fn get_context(&mut self) -> Result<std::result::Result<Resource<UsbContext>, ()>> {
-        // make a context and return it, if the context already exists, return it
-        Result::Ok(Err(()))
-    }
-}
-
 impl IoView for MyState {
     fn table(&mut self) -> &mut ResourceTable {
         &mut self.table
@@ -335,6 +60,213 @@ impl IoView for MyState {
 impl WasiView for MyState {
     fn ctx(&mut self) -> &mut WasiCtx {
         &mut self.ctx
+    }
+}
+
+impl LibusbError {
+    /// Convert a raw `libusb_error` integer value to a `LibusbError` variant.
+    pub fn from_raw(value: i32) -> Self {
+        match value {
+            -1 => LibusbError::Io,
+            -2 => LibusbError::InvalidParam,
+            -3 => LibusbError::Access,
+            -4 => LibusbError::NoDevice,
+            -5 => LibusbError::NotFound,
+            -6 => LibusbError::Busy,
+            -7 => LibusbError::Timeout,
+            -8 => LibusbError::Overflow,
+            -9 => LibusbError::Pipe,
+            -10 => LibusbError::Interrupted,
+            -11 => LibusbError::NoMem,
+            -12 => LibusbError::NotSupported,
+            -99 => LibusbError::Other,
+            _ => LibusbError::Other, // Default to `Other` for unknown error codes
+        }
+    }
+
+}
+
+impl component::usb::configuration::Host for MyState {}
+impl component::usb::descriptors::Host for MyState {}
+impl component::usb::errors::Host for MyState {}
+
+impl HostTransfer for MyState {
+    async fn submit_transfer(&mut self, self_: Resource<Transfer>, data: Vec<u8>) -> Result<std::result::Result<(), component::usb::transfers::LibusbError>> {
+        todo!()
+    }
+
+    async fn cancel_transfer(&mut self, self_: Resource<UsbTransfer>) -> Result<std::result::Result<(), LibusbError>> {
+        todo!()
+    }
+
+    async fn await_transfer(&mut self, self_: Resource<Transfer>) -> Result<std::result::Result<Vec<u8>, component::usb::transfers::LibusbError>> {
+        todo!()
+    }
+
+    async fn drop(&mut self, rep: Resource<UsbTransfer>) -> Result<()> {
+        if let Ok(transfer) = self.table.get(&rep) {
+            unsafe {
+                if !transfer.completed.load(Ordering::SeqCst) {
+                    let _ = libusb_cancel_transfer(transfer.transfer);
+                } else {
+                    // If the transfer is already completed, we can safely drop it
+                    // without calling `libusb_cancel_transfer`.
+                }
+            }
+        }
+        Ok(())
+    }
+}
+
+impl component::usb::transfers::Host for MyState {}
+
+
+impl HostUsbDevice for MyState {
+    async fn open(&mut self, self_: Resource<UsbDevice>) -> Result<std::result::Result<Resource<UsbDeviceHandle>, LibusbError>> {
+        todo!()
+    }
+    
+    async fn get_configuration_descriptor(&mut self, self_: Resource<UsbDevice>, config_index: u8) -> Result<std::result::Result<ConfigurationDescriptor, LibusbError>> {
+        todo!()
+    }
+
+    async fn get_configuration_descriptor_by_value(&mut self, self_: Resource<UsbDevice>, config_value: u8) -> Result<std::result::Result<component::usb::device::ConfigurationDescriptor, component::usb::device::LibusbError>> {
+        todo!()
+    }
+
+    async fn drop(&mut self, rep: Resource<UsbDevice>) -> Result<()> {
+        if let Ok(device) = self.table.get(&rep) {
+            unsafe {
+                libusb_unref_device(device.device);
+            }
+        }
+        Ok(())
+    }
+}
+
+impl HostDeviceHandle for MyState {
+    async fn get_configuration(&mut self, self_: Resource<UsbDeviceHandle>) -> Result<std::result::Result<u8, LibusbError>> {
+        todo!()
+    }
+
+    async fn set_configuration(&mut self, self_: Resource<UsbDeviceHandle>, config: ConfigValue) -> Result<std::result::Result<(), LibusbError>> {
+        todo!()
+    }
+
+    async fn claim_interface(&mut self, self_: Resource<UsbDeviceHandle>, ifac: u8) -> Result<std::result::Result<(), LibusbError>> {
+        todo!()
+    }
+
+    async fn release_interface(&mut self, self_: Resource<UsbDeviceHandle>, ifac: u8) -> Result<std::result::Result<(), LibusbError>> {
+        todo!()
+    }
+
+    async fn set_interface_altsetting(&mut self, self_: Resource<UsbDeviceHandle>, ifac: u8, alt_setting: u8) -> Result<std::result::Result<(), LibusbError>> {
+        todo!()
+    }
+
+    async fn clear_halt(&mut self, self_: Resource<UsbDeviceHandle>, endpoint: u8) -> Result<std::result::Result<(), LibusbError>> {
+        let usb_device_handle = self.table.get(&self_).expect("Failed to get device handle");
+        unsafe {
+            let res = libusb_clear_halt(usb_device_handle.handle, endpoint);
+            Ok(
+                match res {
+                    0.. => Ok(()),
+                    _ => Err(LibusbError::from_raw(res))
+                }
+            )
+        }
+    }
+
+    async fn reset_device(&mut self, self_: Resource<UsbDeviceHandle>) -> Result<std::result::Result<(), LibusbError>> {
+        let usb_device_handle = self.table.get(&self_).expect("Failed to get device handle");
+        unsafe {
+            let res = libusb_reset_device(usb_device_handle.handle);
+            Ok(
+                match res {
+                    0.. => Ok(()),
+                    _ => Err(LibusbError::from_raw(res))
+                }
+            )
+        }
+    }
+
+    async fn kernel_driver_active(&mut self, self_: Resource<UsbDeviceHandle>, ifac: u8) -> Result<std::result::Result<bool, LibusbError>> {
+        let usb_device_handle = self.table.get(&self_).expect("Failed to get device handle");
+        unsafe {
+            let res = libusb_kernel_driver_active(usb_device_handle.handle, ifac as i32);
+            Ok(match res { 
+                0 => Ok(false),
+                1.. => Ok(true),
+                _ => Err(LibusbError::from_raw(res))
+            })
+        }
+    }
+
+    async fn detach_kernel_driver(&mut self, self_: Resource<UsbDeviceHandle>, ifac: u8) -> Result<std::result::Result<(), LibusbError>> {
+        let usb_device_handle = self.table.get(&self_).expect("Failed to get device handle");
+        unsafe {
+            let res = libusb_detach_kernel_driver(usb_device_handle.handle, ifac as i32);
+            Ok(
+                match res {
+                    0.. => Ok(()),
+                    _ => Err(LibusbError::from_raw(res))
+                }
+            )
+        }
+    }
+
+    async fn attach_kernel_driver(&mut self, self_: Resource<UsbDeviceHandle>, ifac: u8) -> Result<std::result::Result<(), LibusbError>> {
+        let usb_device_handle = self.table.get(&self_).expect("Failed to get device handle");
+        unsafe {
+            let res = libusb_attach_kernel_driver(usb_device_handle.handle, ifac as i32);
+            Ok(
+                match res {
+                    0.. => Ok(()),
+                    _ => Err(LibusbError::from_raw(res))
+                }
+            )
+        }
+    }
+
+    async fn close(&mut self, self_: Resource<UsbDeviceHandle>) -> Result<()> {
+        if let Ok(handle) = self.table.get(&self_) {
+            unsafe {
+                libusb_close(handle.handle);
+            }
+        }
+        Ok(())
+    }
+
+    async fn drop(&mut self, rep: Resource<UsbDeviceHandle>) -> Result<()> {
+        if let Ok(handle) = self.table.get(&rep) {
+            unsafe {
+                libusb_close(handle.handle);
+            }
+        }
+        Ok(())
+    }
+
+    async fn alloc_streams(&mut self, self_: Resource<UsbDeviceHandle>, num_streams: u32, endpoints: Vec<u8>) -> Result<std::result::Result<u32, component::usb::device::LibusbError>> {
+        todo!()
+    }
+
+    async fn free_streams(&mut self, self_: Resource<UsbDeviceHandle>, endpoints: Vec<u8>) -> Result<std::result::Result<(), component::usb::device::LibusbError>> {
+        todo!()
+    }
+
+    async fn new_transfer(&mut self, self_: Resource<UsbDeviceHandle>, xfer_type: component::usb::device::TransferType, setup: TransferSetup, buf_size: u32, opts: TransferOptions) -> Result<std::result::Result<Resource<component::usb::device::Transfer>, component::usb::device::LibusbError>> {
+        todo!()
+    }
+}
+
+impl component::usb::device::Host for MyState {
+    async fn init(&mut self) -> Result<std::result::Result<(), component::usb::device::LibusbError>> {
+        todo!()
+    }
+
+    async fn list_devices(&mut self) -> Result<std::result::Result<Vec<Resource<UsbDevice>>, component::usb::device::LibusbError>> {
+        todo!()
     }
 }
 
@@ -350,7 +282,7 @@ async fn main() -> Result<()> {
     let engine = Engine::new(Config::new().async_support(true).wasm_component_model_async(true))?;
     let component = Component::from_file(&engine, component_path)?;
     let mut linker = Linker::new(&engine);
-    Imports::add_to_linker(&mut linker, |state: &mut MyState| state)?;
+    Host_::add_to_linker(&mut linker, |state: &mut MyState| state)?;
     wasmtime_wasi::add_to_linker_async(&mut linker)?;
     let mut store = Store::new(&engine, MyState::new());
     let command = Command::instantiate_async(&mut store, &component, &linker).await?;
@@ -360,6 +292,6 @@ async fn main() -> Result<()> {
 
 /*
     libusb bekijken webusb, taal die programma zelf gebruken zie compileren naar webusb
-    gelijkaardige backend voor libusb voor wasi usb
+    gelijkaardige backend voor libusb voor wasi en wasm
     applicatie compileren met libusb compilenren naar wasi en wasm
  */
